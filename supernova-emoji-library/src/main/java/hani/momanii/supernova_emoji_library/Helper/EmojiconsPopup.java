62380f5be6348090c8e35b61ee1a90b8cb756a63
Meeks91 <meekman2010@gmail.com>
2016-05-27 00:49:30 +0100

    Update EmojiconsPopup.java

--- supernova-emoji-library/src/main/java/hani/momanii/supernova_emoji_library/Helper/EmojiconsPopup.java
+++ supernova-emoji-library/src/main/java/hani/momanii/supernova_emoji_library/Helper/EmojiconsPopup.java
@@ -265,18 +265,18 @@ public class EmojiconsPopup extends PopupWindow implements ViewPager.OnPageChang
             onPageSelected(positionPager);
             if(!isShowing()){
 
-                //If keyboard is visible, simply show the emoji popup
-            //     if(isKeyBoardOpen()){
-            //         showAtBottom();
-            //         // changeEmojiKeyboardIcon(emojiButton, R.drawable.ic_action_keyboard);
-            //     }
+               // If keyboard is visible, simply show the emoji popup
+                if(isKeyBoardOpen()){
+                //    showAtBottom();
+                    // changeEmojiKeyboardIcon(emojiButton, R.drawable.ic_action_keyboard);
+                }
 
-            //     //else, open the text keyboard first and immediately after that show the emoji popup
-            //     else{
-            //         showAtBottomPending();
-            //         // changeEmojiKeyboardIcon(emojiButton, R.drawable.ic_action_keyboard);
-            //     }
-            // }
+                //else, open the text keyboard first and immediately after that show the emoji popup
+                else{
+               //     showAtBottomPending();
+                    // changeEmojiKeyboardIcon(emojiButton, R.drawable.ic_action_keyboard);
+                }
+            }
 
 
 
